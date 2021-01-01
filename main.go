548b90d8a859c7760c80790ea2db4a8c63cd7790
Luke Mann <luke@Lukes-MBP.lan>
2021-01-01 16:46:27 -0500

    aye

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world!")
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/", handler)
 	http.ListenAndServe(":3000", nil)
 }
